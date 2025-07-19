2f6c4f499ef072ac129af68eb6f26b29e58ee89c
Lonmethgkhoi <cantrang83@gmail.com>
2025-07-20 00:55:00 +0700

    index.js

---
+++ Index.js
@@ -0,0 +1,8 @@
+require('dotenv').config(); // nếu bạn có file .env
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.login(process.env.DISCORD_TOKEN);
